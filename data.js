734ab7b2fc830ab6cd028c54972d9ea7a47c7596
Harold Guilline <151622830+guilliharold@users.noreply.github.com>
2026-06-23 16:00:38 +1000

    Update data.js

--- data.js
+++ data.js
@@ -118,7 +118,7 @@ const DEFAULTS = {
   metro_24:       { cars: 12, vans: 6, hwp: 10, trf: 10, ciu: 10, rru: 5, hwp_solo: 2, trf_solo: 2, fviu: 3, socit: 3, dog: 2, sar: 2, sog: 2, cirt: 2, hviu: 3, mounted: 2, cri: 3 },
   metro_non24:    { cars: 7,  vans: 3, hwp: 5,  trf: 5,  ciu: 5,  rru: 3, hwp_solo: 1, trf_solo: 1, fviu: 2, socit: 2, dog: 1, sar: 1, sog: 1, cirt: 1, hviu: 2, mounted: 1, cri: 2 },
   regional_24:    { cars: 6,  vans: 2, hwp: 3,  trf: 3,  ciu: 1,  rru: 0, hwp_solo: 2, trf_solo: 2, fviu: 2, socit: 2, dog: 0, sar: 0, sog: 0, cirt: 0, hviu: 0, mounted: 0, cri: 0 },
-  regional_non24: { cars: 3,  vans: 2, hwp: 2,  trf: 2,  ciu: 2,  rru: 2, hwp_solo: 1, trf_solo: 1, fviu: 1, socit: 1, dog: 1, sar: 1, sog: 1, cirt: 1, hviu: 1, mounted: 1, cri: 1 },
+  regional_non24: { cars: 2,  vans: 1, hwp: 2,  trf: 0,  ciu: 1,  rru: 0, hwp_solo: 0, trf_solo: 0, fviu: 0, socit: 0, dog: 0, sar: 0, sog: 0, cirt: 0, hviu: 0, mounted: 0, cri: 0 },
   regional_single:{ cars: 1,  vans: 1, hwp: 0,  trf: 0,  ciu: 0,  rru: 0, hwp_solo: 0, trf_solo: 0, fviu: 0, socit: 0, dog: 0, sar: 0, sog: 0, cirt: 0, hviu: 0, mounted: 0, cri: 0 },
 };
 
